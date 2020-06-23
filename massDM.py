3988a7b3dd21133a28996209ba67f7335d965789
kingslayer227 <55544508+kingslayer227@users.noreply.github.com>
2020-06-23 17:04:20 +0530

    final commit

--- massDM.py
+++ massDM.py
@@ -1,71 +1,3 @@
-'''
-from discord.ext import commands
-from discord.utils import get
-
-# Bot objects listen for commands on the channels they can "see" and react to them by 
-# calling the function with the same name
-
-bot = commands.Bot(command_prefix='.')
-
-# Here we register the below function with the bot Bot
-# We also specify that we want to pass information about the message containing the command
-# This is how we identify the author
-
-
-
-
-
-
-@bot.command(pass_context=True) 
-
-
-
-
-
-# The command is named MSG
-# The `, *, payload` means take everything after the command and put it in one big string
-
-
-async def MSGG(ctx, *, payload):   
-    # get will return the role if the author has it.  Otherwise, it will return None
-    if get(ctx.message.author.roles, id="649650879741427740"): 
-        for member in ctx.message.server.members:
-            await bot.send_message(member, "THIS IS SEND THROUGH MASS DM BOT...IF RECEIVED CONTACT SLAYER ASAP.")
-
-bot.run('NjQzMTgwNTM4MTQ4MTU5NTI5.XeoNMg.UvGEvqtuIWffDiXOknfm5c90nOk')
-
-
-#####################################DM BOT#############################################################
-
-
-
-
-
-from discord.ext import commands
-from discord.utils import get
-
-
-bot = commands.Bot(command_prefix='.')
-
-
-@bot.command(pass_context=True) 
-
-
-# The command is named MSG
-# The `, *, payload` means take everything after the command and put it in one big string
-
-
-async def MSGG(ctx):   
-    # get will return the role if the author has it.  Otherwise, it will return None
-    if get(ctx.message.author.roles, id="702469635768975360"): 
-        for member in ctx.message.server.members:
-            await bot.dm_channel.send("THIS IS SEND THROUGH MASS DM BOT...IF RECEIVED CONTACT SLAYER ASAP.")
-
-bot.run('NjQzMTgwNTM4MTQ4MTU5NTI5.XeoNMg.UvGEvqtuIWffDiXOknfm5c90nOk')
-
-
-'''
-
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -81,7 +13,7 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command(pass_context=True) 
+@bot.command() 
 
 
 # The command is named MSG
@@ -90,13 +22,19 @@ async def on_ready():
 
 @commands.has_permissions(administrator=True)
 #@bot.command(pass_context = True)
-async def send(ctx, *, content: str):
-        for member in ctx.message.server.members:
-            try:
-                await bot.send_message(member, content)
-                await bot.say("DM Sent To : {} :white_check_mark:  ".format(member))
-            except:
-                print("can't")
-                await bot.say("DM can't Sent To : {} :x: ".format(member))
 
+                
+
+#@bot.command(pass_context=True)
+async def dmtoall(ctx, *, args=None):
+    if args != None:
+        members = ctx.guild.members
+        for member in members:
+            try:
+                await member.send(args)
+                print(" "+ args + " sent to " + member.name + " ")
+            except:
+                print("error")
+    else:
+        await ctx.channel.send("didnt provide arg")
 bot.run('token')
